c2c1fb608ac7de189dbdff9d2a10a3a11706ecdc
originalboss <50785765+originalboss@users.noreply.github.com>
2024-10-05 22:32:43 -0700

    Add files via upload

--- base-apparel-coming-soon-master/sandbox.js
+++ base-apparel-coming-soon-master/sandbox.js
@@ -1,21 +1,45 @@
 let btn = document.querySelector("button");
 let email = document.querySelector("input.email");
+let msg = document.querySelector("p#errormsg"); 
 let realemail =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 
 
 
-function validateEmail(){
+function validateEmail(email){
 
-
-    if(email.value == undefined ||  email.value == null)
+    if(email.value == undefined ||  email.value == "")
 
         {
 
-            let msg = document.querySelector("p#errormsg");
+            let email = document.querySelector("input.email");
+            // let msg = document.querySelector("p#errormsg");
+            let btn = document.querySelector("button");
             msg.innerText = "Please enter your email";
+            email.classList.add("error");
+            btn.classList.add("error"); 
+
             
+
         }
+    else if( !email.value.match(realemail))
+    
+        {   
+        let btn = document.querySelector("button");
+        // let msg = document.querySelector("p#errormsg");
+        msg.innerText = "Please enter a valid email";
+        btn.classList.add("error"); 
+        email.classList.add("error"); 
+    }
+    else{
+        email.classList.add("success");
+        msg.classList.add("success");
+        msg.innerText = "Thanks :)";
+        btn.classList.add("success");
+
+    }
+
+
 
 
 
